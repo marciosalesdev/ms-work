b65a6fb89f654b05534a08d196c6863f9e5679f0
marciosales <marcioserrasales2014@gmail.com>
2023-08-16 20:56:09 -0300

    primeiro commit

---
+++ src/main/java/com/devmarcio/mswork/MsWorkApplication.java
@@ -0,0 +1,13 @@
+package com.devmarcio.mswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmarcio/mswork/MsWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarcio.mswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
